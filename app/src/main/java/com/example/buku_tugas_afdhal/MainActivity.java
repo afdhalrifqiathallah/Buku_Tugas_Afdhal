2fab8c2981a545179035d1972092daccaa7fc8db
Afdhal Rifqi Athallah <afdhalrifqia12@gmail.com>
2023-04-11 17:08:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/buku_tugas_afdhal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.buku_tugas_afdhal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.latihan_layout);
+    }
+}
